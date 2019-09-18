8a0ff604367054de623e8560f974c884d6c46ce8
Woodie Westbrook <wwestbrook@bluestarretirement.com>
2019-09-18 18:01:45 -0400

    vuetify set up

--- src/components/app/App.vue
+++ src/components/app/App.vue
@@ -1,32 +1,60 @@
 <template>
-	<v-app>
-		<v-app-bar app>
-			<v-toolbar-title class="headline text-uppercase">
-				<span>Vuetify</span>
-				<span class="font-weight-light">MATERIAL DESIGN</span>
-			</v-toolbar-title>
-			<v-spacer></v-spacer>
-			<v-btn text href="https://github.com/vuetifyjs/vuetify/releases/latest" target="_blank">
-				<span class="mr-2">Latest Release</span>
-			</v-btn>
-		</v-app-bar>
-
+	<v-app :color="appColors.app">
+		<nav>
+			<v-app-bar app dark class="primary" height="50">
+				<v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+				<v-toolbar-title>
+					KanBAN
+				</v-toolbar-title>
+				<v-spacer></v-spacer>
+			</v-app-bar>
+			<v-navigation-drawer app v-model="drawer">
+				<v-list nav dense>
+					<!-- Home/Dashboard -->
+					<v-list-item :to="{ name: 'Home' }" exact>
+						<v-list-item-icon>
+							<v-icon>mdi-home</v-icon>
+						</v-list-item-icon>
+						<v-list-item-content>
+							<v-list-item-title>Home</v-list-item-title>
+						</v-list-item-content>
+					</v-list-item>
+					<!-- KanBan Board -->
+					<v-list-item :to="{ name: 'KanbanBoard' }" exact>
+						<v-list-item-icon>
+							<v-icon>mdi-view-dashboard-variant</v-icon>
+						</v-list-item-icon>
+						<v-list-item-content>
+							<v-list-item-title>Kanban Board</v-list-item-title>
+						</v-list-item-content>
+					</v-list-item>
+				</v-list>
+			</v-navigation-drawer>
+		</nav>
 		<v-content>
-			<HelloWorld />
+			Woodie was here
 		</v-content>
+		<v-footer app dark class="primary" height="25">
+			<span class="white--text caption">&copy; Jaxnode</span>
+		</v-footer>
 	</v-app>
 </template>
 
 <script>
-import HelloWorld from '../home/HelloWorld.vue';
-
 export default {
-	name: 'App',
-	components: {
-		HelloWorld
-	},
+	name: 'jaxnodeKanBan',
 	data: () => ({
-		//
+		drawer: false,
+		appColors: {
+			app: '#F5F5F5'
+		}
 	})
 };
 </script>
+
+<style scoped lang="scss">
+.nav-link {
+	text-decoration: none;
+	color: inherit;
+}
+</style>
--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -6,5 +6,37 @@ Vue.use(Vuetify);
 export default new Vuetify({
 	icons: {
 		iconfont: 'mdi'
+	},
+	theme: {
+		themes: {
+			light: {
+				primary: '#003a63',
+				secondary: '#0A3854',
+				accent: '#015E84',
+				error: '#D32F2F',
+				info: '#50A2BF',
+				success: '#4CAF50',
+				warning: '#FFA000'
+			},
+			dark: {
+				primary: '#003a63',
+				secondary: '#0A3854',
+				accent: '#015E84',
+				error: '#D32F2F',
+				info: '#50A2BF',
+				success: '#4CAF50',
+				warning: '#FFA000'
+			},
+			/* DO NOT ALTER!! */
+			default: {
+				primary: '#1976D2',
+				secondary: '#424242',
+				accent: '#82B1FF',
+				error: '#FF5252',
+				info: '#2196F3',
+				success: '#4CAF50',
+				warning: '#FFC107'
+			}
+		}
 	}
 });
