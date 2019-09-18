7f9b8a98e57e5356aeaff4f6887176e09608e238
Woodie Westbrook <wwestbrook@bluestarretirement.com>
2019-09-18 17:43:29 -0400

    intial

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,25 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true,
-  },
-  extends: [
-    'plugin:vue/essential',
-    '@vue/airbnb',
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-  },
-  parserOptions: {
-    parser: 'babel-eslint',
-  },
+	root: true,
+	env: {
+		node: true
+	},
+	extends: ['plugin:vue/essential', '@vue/airbnb'],
+
+	rules: {
+		'no-console': 'off',
+		'no-debugger': 'off',
+		quotes: ['error', 'single'],
+		'default-case': 0,
+		'comma-dangle': ['error', 'never'],
+		'max-len': ['error', { code: 140 }],
+		indent: ['error', 'tab'],
+		'no-tabs': 0,
+		'no-param-reassign': 0,
+		'linebreak-style': ['error', 'windows'],
+		'lines-between-class-members': 0
+	},
+
+	parserOptions: {
+		parser: 'babel-eslint'
+	}
 };
--- src/main.js
+++ src/main.js
@@ -1,8 +1,10 @@
 import Vue from 'vue';
-import App from './App.vue';
+import App from './components/app/App.vue';
+import vuetify from './plugins/vuetify';
 
 Vue.config.productionTip = false;
 
 new Vue({
-  render: h => h(App),
+	vuetify,
+	render: h => h(App)
 }).$mount('#app');
